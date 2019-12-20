ea978b19eaa16c2adaa68ba8d9b6eaeb73568b22
gustavo.bruder <gugabruder@gmail.com>
2019-12-20 11:39:35 -0300

    COMMIT PAD_237_ATIVIDADE_13_CRIAR_FUNCIONARIO_PRONTO_API

---
+++ src/main/resources/db/migration/V20191219092032__pad237_seg_funcionarios.sql
@@ -0,0 +1,9 @@
+CREATE TABLE seg_funcionarios
+(
+    id BIGINT   IDENTITY(1, 1)  NOT NULL PRIMARY KEY,
+    nome        VARCHAR(50)     NOT NULL,
+    email       VARCHAR(50)     NOT NULL,
+    uuid        VARCHAR(36)     NOT NULL
+);
+
+CREATE UNIQUE index ix_seg_funcionarios_01 ON seg_funcionarios (email asc);
