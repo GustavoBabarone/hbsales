168956dff07332a3e3bc3f5510c91e45c0b0f114
gustavo.bruder <gustavo.bruder@hbsis.com.br>
2020-06-03 19:54:51 -0300

    Excluindo atendente

--- src/main/java/br/com/hbsis/atendente/IAtendenteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.hbsis.atendente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAtendenteRepository extends JpaRepository<Atendente, Long> {
-}
